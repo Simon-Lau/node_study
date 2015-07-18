59f23d8c090bf9789f2629173032ff3457d86c14
刘平 <tonylp@126.com>
2015-07-18 09:27:29 +0800

    7天学会nodejs

---
+++ 七天学会nodejs/getHello.js
@@ -0,0 +1,8 @@
+var Hello = require('./hello');
+hello = new Hello();
+hello.setName('Simon Lau');
+hello.sayHello();
+
+hello_2 = new Hello();
+hello_2.setName('Tony Lp');
+hello_2.sayHello();
---
+++ 七天学会nodejs/readfile.js
@@ -0,0 +1,17 @@
+/**
+* readfile async
+var fs =require('fs');
+fs.readFile('file.txt','utf-8',function(err, data){
+	if(err){
+		console.error(err);
+	}else{
+		console.log(data);
+	}
+});
+console.log('end.');
+*/
+
+var fs = require('fs');
+var data = fs.readFileSync("file.txt", "utf-8");
+console.log(data);
+console.log('end');
